598b19dff2b303b91e80d273d93c430bed20a270
Srijan Nayak <srijannayak0@gmail.com>
2022-02-15 23:00:45 +0530

    feat: Add initial routing for products page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsListComponent } from './product/products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
