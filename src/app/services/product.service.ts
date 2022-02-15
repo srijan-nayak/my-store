c1fcf2fcfb159d245736c1b8d4bc68787b7d9605
Srijan Nayak <srijannayak0@gmail.com>
2022-02-15 19:21:09 +0530

    feat: Create product service for getting list of products

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>('assets/data.json');
+  }
+}
