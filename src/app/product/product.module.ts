38595d5b07d1541a3fb8ce2fa2c3fff64bf97893
Srijan Nayak <srijannayak0@gmail.com>
2022-02-18 19:43:49 +0530

    feat: Create ProductCardComponent

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
-  declarations: [ProductsListComponent],
+  declarations: [ProductsListComponent, ProductCardComponent],
   imports: [CommonModule],
 })
 export class ProductModule {}
