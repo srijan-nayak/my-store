28718be3d0072c3531b0dd02e12bff19f771446a
Srijan Nayak <srijannayak0@gmail.com>
2022-02-19 17:38:05 +0530

    feat: Create and style NavBarComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductModule } from './product/product.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavBarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, ProductModule],
   providers: [],
   bootstrap: [AppComponent],
