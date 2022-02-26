7bdaadcc94cba7a0440835ecd9ab009cf36a41b3
Srijan Nayak <srijannayak0@gmail.com>
2022-02-26 16:52:42 +0530

    chore: Remove product module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductModule } from './product/product.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsListComponent } from './product/products-list/products-list.component';
+import { ProductCardComponent } from './product/product-card/product-card.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ProductsListComponent,
+    ProductCardComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ProductModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
