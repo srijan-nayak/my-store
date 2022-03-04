84e07349f14fe77144947643eea396294c257344
Srijan Nayak <srijannayak0@gmail.com>
2022-03-04 12:37:51 +0530

    fix: Unsubscribe subscription when ProductsListComponent is destroyed

--- src/app/product/products-list/products-list.component.ts
+++ src/app/product/products-list/products-list.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product, ProductService } from '../../services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css'],
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
   products: Product[] = [];
+  productsSubscription?: Subscription;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService
+    this.productsSubscription = this.productService
       .getProducts()
       .subscribe((products) => (this.products = products));
+  }
+
+  ngOnDestroy() {
+    this.productsSubscription?.unsubscribe();
   }
 }
