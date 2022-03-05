d4a21c64b6db0dc7e7a9da97ab4f0545e447c794
Srijan Nayak <srijannayak0@gmail.com>
2022-03-05 19:20:14 +0530

    feat: Add route for product details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsListComponent } from './product/products-list/products-list.component';
+import { ProductDetailsComponent } from './product/product-details/product-details.component';
 
 const routes: Routes = [
   {
     path: 'products',
     component: ProductsListComponent,
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent,
   },
   { path: '', redirectTo: 'products', pathMatch: 'full' },
 ];
