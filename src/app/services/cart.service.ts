eba32206310760f26daef143ad7a584c3fa71cc7
Srijan Nayak <srijannayak0@gmail.com>
2022-03-07 12:07:47 +0530

    feat: Create totalAmount getter in cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -18,6 +18,12 @@ export class CartService {
     return this._cartItems;
   }
 
+  get totalAmount(): number {
+    return this._cartItems
+      .map(({ product, quantity }) => product.price * quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+
   updateCart(product: Product, quantity: number): void {
     const foundProduct = this._cartItems.find(
       (cartItem) => cartItem.product.id === product.id
